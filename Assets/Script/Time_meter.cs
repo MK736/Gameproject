5cf52db3ef4e69bdedada1c7bc9ff460f39a7e6f
oborodayo <kiyofumi.324@gmail.com>
2023-12-20 15:48:28 +0900

    ver54

---
+++ Assets/Script/Time_meter.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Time_meter : MonoBehaviour
+{
+    public Image UIobj;
+    public bool roop;
+    public float countTime = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (roop)
+        {
+            UIobj.fillAmount -= 1.0f / countTime * Time.deltaTime;
+        }
+    }
+}
